9eda775c3e0328207a09b74a1feccd203b447ce3
Piotr Lesnicki <piotr.lesnicki@atos.net>
2016-05-04 16:18:01 +0200

    query binding of pids for better performance

--- src/topoinfo.c
+++ src/topoinfo.c
@@ -87,11 +87,45 @@ int opal_hwloc_base_cset2mapstr(char *str, int len,
     return OPAL_SUCCESS;
 }
 
-char *get_binding_openmpi(hwloc_cpuset_t cpuset) {
+int topo_init(hwloc_topology_t *topo_p, char* topofile) {
+    hwloc_topology_init(topo_p);
 
-    hwloc_topology_t topo;
-    hwloc_topology_init(&topo);
-    hwloc_topology_load(topo);
+    /* hwloc_topology_ignore_type(*topo_p, HWLOC_OBJ_CACHE); */
+    hwloc_topology_ignore_type(*topo_p, HWLOC_OBJ_MISC);
+    /* hwloc_topology_ignore_type(*topo_p, HWLOC_OBJ_NODE); */
+    hwloc_topology_ignore_type(*topo_p, HWLOC_OBJ_MACHINE);
+    hwloc_topology_ignore_type(*topo_p, HWLOC_OBJ_SYSTEM);
+    hwloc_topology_set_flags(*topo_p, HWLOC_TOPOLOGY_FLAG_WHOLE_SYSTEM);
+
+    if (topofile != NULL) {
+        if (0 != hwloc_topology_set_xml(*topo_p, topofile)) {
+            fprintf(stderr, "topofile failed\n");
+        }
+    }
+
+    return hwloc_topology_load(*topo_p);
+}
+
+char *get_binding_openmpi(hwloc_cpuset_t cpuset, hwloc_topology_t topo) { //char *topofile) {
+
+    /* hwloc_topology_t topo; */
+    /* hwloc_topology_init(&topo); */
+
+    /* hwloc_topology_ignore_type(topo, HWLOC_OBJ_CACHE); */
+    /* hwloc_topology_ignore_type(topo, HWLOC_OBJ_MISC); */
+    /* hwloc_topology_ignore_type(topo, HWLOC_OBJ_NODE); */
+    /* hwloc_topology_ignore_type(topo, HWLOC_OBJ_MACHINE); */
+    /* hwloc_topology_ignore_type(topo, HWLOC_OBJ_SYSTEM); */
+    /* hwloc_topology_set_flags(topo, HWLOC_TOPOLOGY_FLAG_WHOLE_SYSTEM); */
+
+    /* if (topofile != NULL) { */
+    /*     if (0 != hwloc_topology_set_xml(topo, topofile)) { */
+    /*         fprintf(stderr, "topofile failed\n"); */
+    /*     } */
+    /* } */
+
+    /* hwloc_topology_load(topo); */
+
     /* hwloc_cpuset_t cpuset = hwloc_bitmap_alloc(); */
     /* hwloc_get_cpubind(topo, cpuset, HWLOC_CPUBIND_THREAD); */
 
@@ -104,12 +138,61 @@ char *get_binding_openmpi(hwloc_cpuset_t cpuset) {
     return str;
 }
 
+char *get_binding(hwloc_cpuset_t cpuset, hwloc_topology_t topo) {
+    int str_size = 512;
+    char *str = malloc(str_size);
+    //hwloc_obj_t obj = hwloc_get_first_largest_obj_inside_cpuset(topo, cpuset);
+    //hwloc_obj_t obj = hwloc_get_first_largest_obj_inside_cpuset(topo, cpuset);
+    int verbose = 0;
+    hwloc_obj_snprintf(str, str_size, topo, obj, NULL, verbose);
+    return str;
+}
+
+
+int main_old(int argc, char *argv[]) {
+    if (argc <= 1) return 1;
+    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+    /* int rc = hwloc_bitmap_taskset_sscanf(cpuset, argv[1]); */
+    /* if (rc != 0) return 1; */
+    int pid = atoi(argv[1]);
+
+    char *topofile = argc > 1 ? argv[2] : NULL;
+
+    hwloc_topology_t topo;
+    topo_init(&topo, topofile);
+
+    hwloc_get_proc_cpubind(topo, pid, cpuset, 0);
+
+    char *report_binding = get_binding_openmpi(cpuset, topo);
+    char *objstr = get_binding(cpuset, topo);
+    char *maskstr;
+    hwloc_bitmap_asprintf(&maskstr, cpuset);
+    printf("%s | %s | %s", report_binding, objstr, maskstr);
+
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     if (argc <= 1) return 1;
     hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
-    int rc = hwloc_bitmap_taskset_sscanf(cpuset, argv[1]);
-    if (rc != 0) return 1;
-    char *report_binding = get_binding_openmpi(cpuset);
-    printf("%s", report_binding);
+    hwloc_topology_t topo;
+    topo_init(&topo, NULL);
+
+    int i;
+    for (i=1; i<argc; i++) {
+        int pid = atoi(argv[i]);
+        hwloc_get_proc_cpubind(topo, pid, cpuset, 0);
+
+        char *report_binding = get_binding_openmpi(cpuset, topo);
+        char *objstr = get_binding(cpuset, topo);
+        char *maskstr;
+        hwloc_bitmap_asprintf(&maskstr, cpuset);
+        printf("%d %s %s %s\n", pid, maskstr, report_binding, objstr);
+
+        free(report_binding);
+        free(objstr);
+        free(maskstr);
+    }
     return 0;
 }
+
